644539b8d91911cae3bf080907b4ee57d9bae3eb
Evgeniy <teralitr@ya.ru>
2019-02-14 19:40:22 +0300

    Added advanced field generation

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Cell : MonoBehaviour {
+    public bool Walkable;
+    
+    [SerializeField] Renderer _renderer;
+    
+    static readonly int colorMatParam = Shader.PropertyToID("_Color");
+    
+    void Awake() {
+        _renderer = GetComponentInChildren<Renderer>();
+        SetVisual(Walkable);
+    }
+
+    void SetVisual(bool walkable) {
+        Color color = walkable ? Color.green : Color.red;
+        _renderer.material.SetColor(colorMatParam, color);
+    }
+}
--- Assets/Scripts/VisualFieldGenerator.cs
+++ Assets/Scripts/VisualFieldGenerator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+
+using UnityEngine;
 
 using Airion.Extensions;
 
@@ -8,29 +10,50 @@ using UnityEditor;
 public class VisualFieldGenerator : MonoBehaviour {
     [SerializeField] GameObject _groundBlockPrefab;
 
-    [SerializeField] int[][] _field;
     [SerializeField] Vector2Int _fieldSize;
     [SerializeField] float _innerRadius = 1;
     [SerializeField] float _outerRadius = 1;
+
+    int[,] _field = new int[,] {
+                                   {0, 0, 0, 0, 1},
+                                   {0, 1, 1, 1, 0},
+                                   {0, 0, 1, 1, 1},
+                                   {0, 1, 1, 1, 0},
+                                   {0, 0, 1, 1, 0}
+                               }; 
     
     [ContextMenu(nameof(GenerateHG))]
     void GenerateHG() {
         transform.DestroyAllChildren();
         for (int i = 0; i < _fieldSize.x; i++) {
             for (int j = 0; j < _fieldSize.y; j++) {
-                var blockPrefab = PrefabUtility.InstantiatePrefab(_groundBlockPrefab) as GameObject;
-                blockPrefab.transform.position = GetPosByGrid(i, j);
-                blockPrefab.transform.SetParent(transform);
+               CreateBlock(i, j);
             }
         }
+    }
+
+    void CreateBlock(int x, int y) {
+        var cellPrefab = PrefabUtility.InstantiatePrefab(_groundBlockPrefab) as GameObject;
+        cellPrefab.transform.position = GetPosByGrid(x, y);
+        cellPrefab.transform.SetParent(transform);
+        cellPrefab.GetComponent<Cell>().Walkable = GetWalkable(x, y);
     }
  
     Vector3 GetPosByGrid(int i, int j) {
         Vector3 pos = Vector3.zero;
-        
-        pos.x =  (i + j * 0.5f - j / 2) * (_innerRadius * 2f);
-        pos.z = j * _outerRadius * 1.5f;
+        pos.x = j * _innerRadius + ((i % 2) * _innerRadius / 2.0f);
+        pos.z = -i * _outerRadius;
         return pos;
+    }
+
+    bool GetWalkable(int x, int y) {
+        try {
+            return _field[x, y] > 0;
+        }
+        catch (Exception e) {
+            Debug.LogError($"[{GetType()}] {e.Message}");
+            return true;
+        }
     }
 }
 
